97d4f4686ec9112f7d81b63da4426769727222c9
DDOGYEOM <ehrua5@naver.com>
2019-08-19 15:40:19 +0900

    asdasd

---
+++ src/Day.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="day">
+        <div class="date"><span>{{day.date.getDate()}}</span> <span class="month">({{day.date.getMonth()+1}}월)</span></div>
+        <div class="list">
+            <div class="item" v-for="todo in day.list">{{todo.name}}</div>
+        </div>
+    </div>
+</template>
+
+    
+<script>
+    export default {
+        name:'day',
+        props:['day'],
+        data(){
+            return {
+                
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .day {
+        width:100%;
+        height:100%;
+        display: flex;
+        justify-content: flex-start;
+        flex-direction: column;
+        box-shadow: 2px 2px 2px 2px #ddd;
+    }
+
+    .date {
+        margin: 10px 10px; 
+    }
+
+    .date > span:nth-child(1) {
+        font-size: 24px;
+    }
+
+
+
+
+</style>
